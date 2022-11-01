a4c8bbbba93c87e027d14fff41eb443d58ae93d8
Steve Crow <steve@smcrow.com>
2022-11-01 13:22:27 -0400

    Better error handling.

--- src/routes/[station]/+page.svelte
+++ src/routes/[station]/+page.svelte
@@ -5,15 +5,21 @@
 	import axios from 'axios';
 
 	let metar = '';
+
 	async function fetchMetar() {
 		const station = $page.params.station;
-		if (station) {
-			return (await axios.get(`/${station}`)).data.raw;
+		try {
+			if (station) {
+				return (await axios.get(`/${station}`)).data.raw;
+			}
+		} catch (err) {
+			return 'ERROR: PROBLEM FETCHING DATA FROM AVWX';
 		}
 
 		// This shouldn't happen because of how Svelte routes things
 		return 'ERROR: NO STATION';
 	}
+
 	onMount(async () => {
 		const interval = setInterval(async () => {
 			metar = await fetchMetar();
