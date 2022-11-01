84331e3a4f6b476817d7bb08c68f90e18aca2768
Steve Crow <steve@smcrow.com>
2022-11-01 14:28:41 -0400

    Change URLs around.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
+
+	import axios from 'axios';
+
+	const stations = $page.url.searchParams.get('stations') || null;
+	const accessKey = $page.url.searchParams.get('accessKey') || null;
+
+	let metars = [];
+	let message = null;
+
+	async function fetchMetars() {
+		try {
+			if (stations !== null && accessKey !== null) {
+				const responseData = (await axios.get(`/api?accessKey=${accessKey}&stations=${stations}`))
+					.data;
+				if (responseData.metars.length > 0) {
+					return { message: null, metars: responseData.metars };
+				}
+			}
+
+			// This shouldn't happen because of how Svelte routes things
+			return { message: 'ERROR: NO STATION', metars: [] };
+		} catch (err) {
+			return { message: `ERROR: PROBLEM FETCHING DATA FROM AVWX`, metars: [] };
+		}
+	}
+
+	onMount(async () => {
+		const response = await fetchMetars();
+		metars = response.metars;
+		message = response.message;
+	});
+</script>
+
+{#if message}
+	<div class="metar-wrapper">MESSAGE: {message}</div>
+{:else}
+	{#each metars as metar}
+		<div class="metar-wrapper">{metar.raw}</div>
+	{/each}
+{/if}
---
+++ src/routes/api/+server.ts
@@ -0,0 +1,37 @@
+import { json } from '@sveltejs/kit';
+import type { RequestEvent } from '@sveltejs/kit';
+import { AVWX_API_KEY } from '$env/static/private';
+import axios from 'axios';
+
+/**
+ * This method proxies the client API requests to the weather server.
+ *
+ */
+export async function GET({ url }: RequestEvent) {
+	const stations = url.searchParams.get('stations')?.split(',') ?? ([] as string[]);
+	const accessKey = url.searchParams.get('accessKey');
+	try {
+		const metars = [];
+		if (stations.length > 0 && accessKey !== null) {
+			for (const station of stations) {
+				const metar = await getSingle(station, accessKey);
+				metars.push(metar);
+			}
+
+			return json({ message: null, metars });
+		}
+
+		return json({ message: 'Station and access key are required' });
+	} catch (err) {
+		return json({ message: 'Error Communicating with API' });
+	}
+}
+
+async function getSingle(station: string, accessKey: string) {
+	const response = await axios.get(`https://avwx.rest/api/metar/${station}`, {
+		headers: {
+			Authorization: `Bearer ${accessKey}`
+		}
+	});
+	return response.data;
+}
